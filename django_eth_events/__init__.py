47199d7983adccd4186bbd982fd4593448983d1c
enth <chabanenko.sergey@gmail.com>
2022-04-21 11:47:24 +0500

    Add version check for default_app_config

--- django_eth_events/__init__.py
+++ django_eth_events/__init__.py
@@ -1,2 +1,7 @@
+import django
 from .apps import app as celery_app
+
+if django.VERSION < (3, 2):
+    default_app_config = 'django_eth_events.apps.DjangoEthEventsConfig'
+
 __all__ = ['celery_app']
